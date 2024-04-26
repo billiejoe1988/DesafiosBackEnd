5f3d92dce6c210aa632f4d9a87482ff09a74851d
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-04-26 20:31:43 -0300

    ADD, desafio 3, server, json de products y manager basado en el desafio 2

--- desafio03/src/manager/manager.js
+++ desafio03/src/manager/manager.js
@@ -10,6 +10,146 @@
 // minimo archivo con 10 productos creados, para q los tutores no tengan que crear archivos 
 // puerto 8080
 // mandan a llamar localhost:8080/products sin query y debe devolver los 10 productos
-// se mandan a llamar desde el navegador  localhost:8080/products/products?limit=5, eso debe devolver solo los primeros 5 de 10.
+// se mandan a llamar desde el navegador  localhost:8080/products?limit=5, eso debe devolver solo los primeros 5 de 10.
 //llama localhost:8080/products/2 se deolvueve solo el id 2
 // mandan a llamar desde navegador localhost:8080/products/2343243 y tiene q indicar que no existe.
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    async getProducts() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const productsData = await fs.promises.readFile(this.path, 'utf8');
+                if (productsData.trim() === '') return []; 
+                return JSON.parse(productsData);
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async createProduct(title, description, price, thumbnail, code, stock) {
+        try {
+            const products = await this.getProducts();
+
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                console.log("Error: Todos los campos son obligatorios.");
+                return;
+            }
+
+            if (products.some(product => product.code === code)) {
+                console.log("Error, Code repetido");
+                return;
+            }
+
+            const product = {
+                id: uuidv4(),
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+            }
+
+            products.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const products = await this.getProducts();
+            const product = products.find(product => product.id === id);
+            if (!product) {
+                console.log("Product not found");
+            }
+            return product;
+        } catch (error) {
+            console.log("Error:", error.message); 
+            return null;
+        }
+    }
+
+    async deleteFile(){
+        try{
+            await fs.promises.unlink(this.path);
+            console.log("archivo eliminado");
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            const products = await this.getProducts();
+            const i = products.findIndex(product => product.id === id);
+            if (i === -1) {
+                throw new Error("Product not found");
+            }
+            console.log(`${products[i].title} ha sido borrado`); 
+            products.splice(i, 1); 
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    
+    async modifyProduct(id, title, description, price, thumbnail, code, stock) {
+        try {
+            const products = await this.getProducts();
+    
+            const i = products.findIndex(product => product.id === id);
+            if (i === -1) {
+                throw new Error("Product not found");
+            }
+    
+            products[i] = {
+                id: id,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+            }            
+    
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+            return products[i];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const productManager = new ProductManager ('./products.json') ;
+
+//--------------------------------------------------------------------------------------------
+
+const armarDB = async () => {
+    console.log("--------------- creando 15 productos en la bd ");
+
+    for (let i = 1; i <= 15; i++) {
+        await productManager.createProduct(`Producto ${i}`, `Este es el producto ${i}`, 200 + i, `Imagen ${i}`, `abc${i}`, 25 + i);
+    }
+
+    console.log("------------------------------------nuevo contenido del array");
+    console.log(await productManager.getProducts());
+}
+
+armarDB();
+
+//-------------------------------------------------------------------------------------------
--- desafio03/src/server.js
+++ desafio03/src/server.js
@@ -1,10 +1,76 @@
-import express from "express";
+import express from  'express';
+import ProductManager from './manager/manager.js';
 
 const app = express();
+const productManager = new ProductManager('./products.json');
 
-app.get('/', (request, response) => {
-    response.json(products);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send ('<h1> Products Manager Server </h1>')
 });
+
+app.get('/products', async (req, res) => {
+    try{
+        const { limit } = req.query;
+        let products = await productManager.getProducts();
+        if (limit){
+            const limitInt = parseInt(limit);
+            if (!isNaN(limitInt) && limitInt > 0) {
+              products = products.slice(0, limitInt);
+            }
+        }
+        res.status(200).json(products)
+    } catch (error){
+        res.status(500).json({msg: error.message })
+    }
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const products = await productManager.getProducts();
+        const product = products.find(p => p.id === id);
+        if (!product) {
+            res.status(404).json({msg: 'product not found '});
+        } else {
+            res.status(200).json(product);
+        } 
+    } catch (error) {
+        res.status(500).json({msg:error.message});
+    }
+})
+
+app.post('/products', async (req, res) => {
+    try {
+        const { title, description, price, thumbnail, code, stock } = req.body;
+        await productManager.createProduct(title, description, price, thumbnail, code, stock);
+        res.status(200).json({ msg: "Producto creado correctamente" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+app.put('/products/:id', async(req, res) => {
+    const { id } = req.params;
+    try {
+        const { title, description, price, thumbnail, code, stock } = req.body;
+        await productManager.modifyProduct(id, title, description, price, thumbnail, code, stock);
+        res.status(200).json({ msg: "Producto modificado correctamente" });
+    } catch (error) {
+        res.status(500).json ({msg: error.message})
+    }
+})
+
+app.delete('/products/:id', async(req, res) => {
+    const { id } = req.params;
+    try {
+        await productManager.deleteProduct(id);
+        res.status(200).json({ msg: "Producto eliminado correctamente" });
+    } catch (error) {
+        res.status(500).json ({msg: error.message})
+    }
+})
 
 const PORT = 8080;
 
