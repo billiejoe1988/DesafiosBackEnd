aabf6a208a53082853ee3d5ebf8f8d9c916a9cf8
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-05-07 03:07:17 -0300

    ADD, 1ra, pre entrega - product & cart manager - router

---
+++ PreEntrega01/src/manager/manager.js
@@ -0,0 +1,144 @@
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+
+export default class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    async getProducts() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const productsData = await fs.promises.readFile(this.path, 'utf8');
+                if (productsData.trim() === '') return []; 
+                return JSON.parse(productsData);
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async createProduct(product) {
+        try {
+            const { title, description, price, thumbnail, code, stock, category } = product;
+
+            if (!title || !description || !price || !thumbnail || !code || !stock || !category) {
+                throw new Error("Todos los campos son obligatorios.");
+            }
+
+            const products = await this.getProducts();
+
+            if (products.some(existingProduct => existingProduct.code === code)) {
+                throw new Error("El código del producto ya existe.");
+            }
+
+            const newProduct = {
+                pid: uuidv4(), 
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+                category,
+                status: true 
+            };
+
+            products.push(newProduct);
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+
+            return newProduct;
+        } catch (error) {
+            throw new Error(`Error al crear el producto: ${error.message}`);
+        }
+    }
+
+    async getProductById(pid) {
+        try {
+            const products = await this.getProducts();
+            const product = products.find(product => product.pid === pid);
+            if (!product) {
+                console.log("Product not found");
+            }
+            return product;
+        } catch (error) {
+            console.log("Error:", error.message); 
+            return null;
+        }
+    }
+
+    async deleteFile(){
+        try{
+            await fs.promises.unlink(this.path);
+            console.log("archivo eliminado");
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteProduct(pid) {
+        try {
+            const products = await this.getProducts();
+            const i = products.findIndex(product => product.pid === pid);
+            if (i === -1) {
+                throw new Error("Product not found");
+            }
+            console.log(`${products[i].title} ha sido borrado`); 
+            products.splice(i, 1); 
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    async modifyProduct(pid, title, description, price, code, stock, category, status, thumbnail = null) {
+        try {
+            const products = await this.getProducts();
+    
+            const i = products.findIndex(product => product.pid === pid);
+            if (i === -1) {
+                throw new Error("Product not found");
+            }
+    
+            const updatedProduct = {
+                pid: pid,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+                status: status !== undefined ? status : true,
+            };
+    
+            products[i] = updatedProduct;
+    
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+            return updatedProduct;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const productManager = new ProductManager ('./products.json') ;
+
+//--------------------------------------------------------------------------------------------
+
+const armarDB = async () => {
+    console.log("--------------- creando 15 productos en la bd ");
+
+    for (let i = 1; i <= 15; i++) {
+        await productManager.createProduct(`Producto ${i}`, `Este es el producto ${i}`, 200 + i, `Imagen ${i}`, `abc${i}`, 25 + i);
+    }
+
+    console.log("------------------------------------nuevo contenido del array");
+    console.log(await productManager.getProducts());
+}
+
+
+//-------------------------------------------------------------------------------------------
---
+++ PreEntrega01/src/routes/cart.router.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import CartManager from "../manager/cart.manager.js"; 
+
+const routerCart = Router();
+const cartManager = new CartManager("./carts.json");
+
+routerCart.get('/', async (req, res) => {
+    try {
+        let limit = req.query.limit; 
+        let carts = await cartManager.getCarts();
+
+        if (limit && !isNaN(parseInt(limit))) {
+            carts = carts.slice(0, parseInt(limit)); 
+        }
+
+        res.status(200).json(carts);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+routerCart.post('/:cid/product/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        const { quantity } = req.body;
+        let cart;
+
+        const existingCart = await cartManager.getCartById(cid);
+        
+        if (existingCart) {
+            cart = await cartManager.addToCart(cid, { pid, quantity });
+        } else {
+            cart = await cartManager.createCart({ pid, quantity });
+        }
+
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+routerCart.get('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const cart = await cartManager.getCartById(cid);
+
+        if (!cart) {
+            return res.status(404).json({ msg: "Cart not found" });
+        }
+
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+export default routerCart;
